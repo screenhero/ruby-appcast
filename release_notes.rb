cef2a9e5d6f9cc12bf65ab004e95f93e2047f94f
Jason DiCioccio <jd@ods.org>
2014-08-08 17:09:07 -0700

    Simple release notes generator (mimic's choctop's)

---
+++ release_notes.rb
@@ -0,0 +1,26 @@
+require 'RedCloth'
+require 'erb'
+
+module Screenhero
+  module Sparkle
+    class ReleaseNotes
+      def initialize(textile, style=nil)
+        @body = RedCloth.new(textile).to_html
+
+        if !style
+          style = <<-EOF
+            * { font-family: sans-serif;}
+            h1 { font-family: sans-serif; background-color: #169cd8; padding: 10px; color: #ffffff;}
+            h1 span { font-size: 75%; font-weight: normal; float: right;}
+          EOF
+        end
+
+        @head = "<head><style>" + style + "</style></head>"
+      end
+
+      def to_s
+        "<html>" + @head + @body + "</html>"
+      end
+    end
+  end
+end
